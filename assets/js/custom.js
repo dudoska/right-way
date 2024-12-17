1e1f08993b4934fa1c4a9761a87bc07f623259bb
dudosa <koleg3307@gmail.com>
2024-12-17 20:52:59 +0300

    Updated code style in custom.js
    Took 2 minutes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
 	"use strict";
-    
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : CarVilla HTML Template
-* Version       : 1.0
-==================================== */
-
-
-
 
 /*=========== TABLE OF CONTENTS ===========
 1. Scroll To Top
@@ -16,7 +7,7 @@ $(document).ready(function(){
 3. owl carousel
 ======================================*/
 
-    // 1. Scroll To Top 
+    // 1. Scroll To Top
 		$(window).on('scroll',function () {
 			if ($(this).scrollTop() > 300) {
 				$('.return-to-top').fadeIn();
@@ -32,7 +23,6 @@ $(document).ready(function(){
 		});
 
 	// 2. welcome animation support
-
         $(window).load(function(){
         	$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
             $(".welcome-hero-txt button").removeClass("animated fadeInDown").css({'opacity':'0'});
@@ -43,11 +33,9 @@ $(document).ready(function(){
             $(".welcome-hero-txt button").addClass("animated fadeInDown").css({'opacity':'0'});
         });
 
-	
-	// 3. owl carousel
 
+	// 3. owl carousel
 		// i.  new-cars-carousel
-		
 			$("#new-cars-carousel").owlCarousel({
 				items: 1,
 				autoplay:true,
@@ -64,23 +52,21 @@ $(document).ready(function(){
 
 
 		// ii. .testimonial-carousel
-	
-		
 			var owl=$('.testimonial-carousel');
 			owl.owlCarousel({
 				items:3,
 				margin:0,
-				
+
 				loop:true,
 				autoplay:true,
 				smartSpeed:1000,
-				
+
 				//nav:false,
 				//navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-				
+
 				dots:false,
 				autoplayHoverPause:false,
-			
+
 				responsiveClass:true,
 					responsive:{
 						0:{
@@ -93,12 +79,11 @@ $(document).ready(function(){
 							items:3
 						}
 					}
-				
-				
+
+
 			});
 
 		// iii. .brand-item (carousel)
-		
 			$('.brand-item').owlCarousel({
 				items:6,
 				loop:true,
@@ -121,8 +106,7 @@ $(document).ready(function(){
 						}
 					}
 				});
-				
-				
+
 				$('.play').on('click',function(){
 					owl.trigger('play.owl.autoplay',[1000])
 				})
